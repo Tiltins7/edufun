bc337837b6090d57936e2217e1edb5430797c45e
RALFS GRZIBOVSKIS <Ralfs.Grzibovskis@alu.uclm.es>
2019-11-29 16:31:00 +0100

    nomainiju visu hardcoded textu uz string failiem, added spanish language

--- app/src/main/java/com/example/edufun/MainActivity.kt
+++ app/src/main/java/com/example/edufun/MainActivity.kt
@@ -1,9 +1,14 @@
-package com.example.edufun
+@file:Suppress("DEPRECATION")
 
+package com.example.edufun
+import java.util.*
+import android.content.Context
+import android.content.res.Configuration
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +28,48 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+
+        langbutton.setOnClickListener{
+
+            showChangeLang()
+
+        }
+
+
+    }
+
+    private fun showChangeLang() {
+        val listItems = arrayOf("english","español")
+        val mBuilder = AlertDialog.Builder(this@MainActivity)
+        mBuilder.setTitle("Choose Language")
+        mBuilder.setSingleChoiceItems(listItems,-1){dialog, number ->
+            if (number == 0){
+                setLocate("en")
+                recreate()
+            }
+            if (number == 1){
+                setLocate("es")
+                recreate()
+            }
+            dialog.dismiss()
+        }
+        val mDialog = mBuilder.create()
+        mDialog.show()
+    }
+
+    private fun setLocate(Lang: String){
+        val locale = Locale(Lang)
+
+        Locale.setDefault(locale)
+
+        val config = Configuration()
+
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        editor.apply()
     }
 }
 
